3a9d46797dbbcd495141133875b85cf9097905b2
Sandaime <ispodeon@yahoo.com>
2020-11-10 20:40:12 -0500

    more updates

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -2,8 +2,11 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'hello',
-  template: `<h1>Hello {{name}}!</h1>`,
-  styles: [`h1 { font-family: Lato; }`]
+  template: `<h1>Hello {{name}}!</h1><p>Help</p>`,
+  styles: [`
+  h1 { font-family: Lato; }
+  p {color: red}
+  `]
 })
 export class HelloComponent  {
   @Input() name: string;
