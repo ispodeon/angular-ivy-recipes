0accf70bd4387f77938042f06812b91330ec9a68
Sandaime <ispodeon@yahoo.com>
2020-12-04 00:27:18 -0500

    recipe detail added

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -10,13 +10,19 @@ export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a Test', 'https://grantourismotravels.com/wp-content/uploads/2020/04' +
     '/Cambodian-Pork-and-Rice-Recipe-Bai-Sach-Chrouk-Copyright-2020-Terence-Carter-Grantourismo.jpg'),
-    new Recipe('A Test Recipe', 'This is simply a Test', 'https://grantourismotravels.com/wp-content/uploads/2020/04' +
+    new Recipe('A Test Recipe 2', 'This is simply a Test', 'https://grantourismotravels.com/wp-content/uploads/2020/04' +
     '/Cambodian-Pork-and-Rice-Recipe-Bai-Sach-Chrouk-Copyright-2020-Terence-Carter-Grantourismo.jpg')
   ];
+
+  @Output() recipeWasSelected: EventEmitter<Recipe> = new EventEmitter<Recipe>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
