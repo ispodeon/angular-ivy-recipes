f85a47964498b46ff1f5f9aa057e1674431a4338
Sandaime <ispodeon@yahoo.com>
2021-04-01 01:04:52 -0400

    added Subject for subscription

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter, Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 import { Recipe } from '../recipes/recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
@@ -6,7 +7,7 @@ import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 @Injectable()
 export class RecipeService {
-    recipeSelected = new EventEmitter<Recipe>();
+    // recipeSelected = new Subject<Recipe>();
 
     private recipes: Recipe[] = [
         new Recipe('A Test Recipe',
